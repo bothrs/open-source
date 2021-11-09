abae665643616776757670d1e0ae0a2e4ce79cc8
Tijs Martens <tijs@bothrs.com>
2021-11-09 15:09:13 +0100

    fix: simplify translations, remove mapping

--- packages/translations/src/index.ts
+++ packages/translations/src/index.ts
@@ -7,7 +7,12 @@ import AsyncStorageBackend from 'i18next-async-storage-backend'
 import MultiloadAdapter from 'i18next-multiload-backend-adapter'
 import Fetch from 'i18next-fetch-backend'
 
-import type { AirtableRecord, FormattedTranslation, Translation, TranslationInitParams, TranslationKeys } from './types'
+import type {
+  FormattedTranslation,
+  Translation,
+  TranslationInitParams,
+  TranslationKeys,
+} from './types'
 
 /**
  * A hook which initializes i18next and loads the languages from an external data source
@@ -42,11 +47,11 @@ function initTranslations({
     allowMultiLoading: true,
     parse: function (data: string) {
       const parsedData = JSON.parse(data)
-      const translationData: FormattedTranslation[] = parsedData.records.map(
-        (record: AirtableRecord) => {
-          return record.fields
-        }
-      )
+      console.log(loadPath)
+
+      console.log(Object.keys(parsedData.translations))
+
+      const translationData: FormattedTranslation[] = parsedData.translations
 
       const langSet = new Set()
 
