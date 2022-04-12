a0b4cd880a2201541cb0f19a65277312f2a61f7b
Thomas Ghysels <info@thomasg.be>
2022-04-12 09:19:10 +0200

    fix: add deprecation warning in container interface
    Resolves #33

--- packages/react-native-layout/src/components/container/Container.props.ts
+++ packages/react-native-layout/src/components/container/Container.props.ts
@@ -5,6 +5,7 @@ import type { PaddingOrMarginProps } from '../../types/generic-props'
 export interface ContainerProps {
   type: 'padding' | 'margin'
   amount: PaddingOrMarginProps
+  /** @deprecated use `style={{ backgroundColor: "red" }}` instead */
   backgroundColor?: string
   style?: ViewStyle
   onLayout?: (e: LayoutChangeEvent) => void
