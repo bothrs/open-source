079f70c0862b0569864eb4c7a057cb70468907d0
Fabian Meul <fabian.meul@gmail.com>
2022-06-02 14:26:40 +0200

    refactor(Util): Refactor FS to use node util

--- packages/util/src/fs.ts
+++ packages/util/src/fs.ts
@@ -3,36 +3,18 @@
  * @module
  */
 
-import { readFile as _readFile, writeFile as _writeFile } from 'fs'
+import { readFile, writeFile } from 'node:fs'
+import * as util from 'node:util'
 
-export function readFile(fileName: string, type = 'utf8'): Promise<string> {
-  return new Promise((resolve, reject) => {
-    _readFile(fileName, type, (error: any, data: any) => {
-      error ? reject(error) : resolve(data)
-    })
-  })
+export const readFileAsync = util.promisify(readFile)
+export const writeFileAsync = util.promisify(writeFile)
+
+export function readJSON<T = Record<string, unknown>>(
+  fileName: string
+): Promise<T> {
+  return readFileAsync(fileName, 'utf8').then((data) => JSON.parse(data))
 }
 
-export function writeFile(fileName: string, data: string, type = 'utf8') {
-  return new Promise((resolve, reject) => {
-    _writeFile(fileName, data, type, (error) => {
-      error ? reject(error) : resolve(data)
-    })
-  })
-}
-
-export function readJSON(fileName: string) {
-  return new Promise((resolve, reject) => {
-    _readFile(fileName, 'utf8', (error, data) => {
-      error ? reject(error) : resolve(JSON.parse(data))
-    })
-  })
-}
-
-export function writeJSON(fileName: string, data: any) {
-  return new Promise((resolve, reject) => {
-    _writeFile(fileName, JSON.stringify(data), 'utf8', (error) => {
-      error ? reject(error) : resolve(data)
-    })
-  })
+export function writeJSON(fileName: string, data: any): Promise<void> {
+  return writeFileAsync(fileName, JSON.stringify(data), 'utf8')
 }
