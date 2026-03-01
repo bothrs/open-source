2bcab09603db26a45fbbdccafbcea856f7e8fe56
Emiel De Vleeschouwer <emiel@nimblestudio.com>
2026-03-01 21:08:01 +0100

    chore: update pipeline

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-}
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"],
+    ],
+    "subject-case": [0],
+  },
+};
